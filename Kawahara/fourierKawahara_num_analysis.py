27ed38d3461b2164bdd511827c2f6ceced478b2a
xli2522 <50414011+xli2522@users.noreply.github.com>
2021-06-27 05:07:48 -0400

    u1 Fourier coefficients calculation

--- Kawahara/fourierKawahara_num_analysis.py
+++ Kawahara/fourierKawahara_num_analysis.py
@@ -23,11 +23,10 @@ def main():
 
     #mus = [5.09,5.48,4.79,5.02,4.16,4.23,3.24,-3.23,2.27,2.36,1.49,1.64,0.74,0.69]
     #lambs = [62.82,51.62,35.98,19.78,7.09,0.595,-0.219,-0.305,-3.22,-13.41,-29.71,-49.13,-64.87,-57.60]
-
-    mus = [0.74]        #test individual cases - large mu
-    lambs = [-64.87]        #test individual cases - large lambda
-    #mus = [0.632]      #test individual cases - small mu
-    #lambs = [0.227]        #test indivvidual cases - small lambda
+    #mus = [0.74]        #test individual cases - large mu
+    #lambs = [-64.87]        #test individual cases - large lambda
+    #mus = [5.09]      #test individual cases - small mu
+    #lambs = [62.82]        #test indivvidual cases - small lambda
 
     std = np.zeros(len(mus))       
     main_start = time.time()
@@ -47,26 +46,25 @@ def main():
         N = int(np.floor(30*L/(2*np.pi)))     #number of spatial steps; fit to the length of the periodic domain
         dx = L / (N - 1.0)      #spatial step size
         x = np.linspace(0, (1-1.0/N)*L, N)      #initialize x spatial axis    
-        print('Periodic Domain (u0&u1): ' + str(L))
-        print('Spatial steps: ' + str(N))
         # Set the time sample grid.
-        T = 1
-        t = np.linspace(0, T, 800)
+        T = 8
+        t = np.linspace(0, T, 1600)
         dt = len(t)
         ######################################################################
 
         param1 = [1, 3/160, 1, 0.01, lambs[i]]      # [alpha, beta, sigma, epsilon, lamb]
         param2 = [0.01, lambs[i], mus[i], 1, 3/160, 1]                # [delta, lamb, mu, alpha, beta, sigma]
-        a1 = 0.0001*param1[3]      #DECREASE A1 WHEN BETA IS LARGE
+        #a1 = 0.0001*param1[3]      #DECREASE A1 WHEN BETA IS LARGE
+        a1 = 0.1*param1[3]      #DECREASE A1 WHEN BETA IS LARGE
         ic_start = time.time()
         leading_terms_u0 = waveEquation.u0_leading_terms(param1, a1)
         stationary_u0 = waveEquation.kawahara_stationary_solution(x, leading_terms_u0, L, param1)
-        leading_terms_u1 = waveEquation.u1_leading_terms(param2)
+        leading_terms_u1 = waveEquation.u1_leading_terms(stationary_u0, param2)     #temp remove param2 to skip matrix calculation
         combined_u =  waveEquation.kawahara_combined_solution(stationary_u0, x, leading_terms_u1, param2, 0.01)
 
         print("Initial condition calculation --- %s seconds ---" % (time.time() - ic_start))
         solver_start = time.time()
-        sol = waveEquation.solve_kawahara(waveEquation.kawahara_model, combined_u, t, L, param1, 5000)
+        sol = waveEquation.solve_kawahara(waveEquation.kawahara_model, combined_u, t, L, param1, 5000, modelArg=(True, False))           #modelArg temporarily not avaliable
         print("Numerical solver --- %s seconds ---" % (time.time() - solver_start))
         print("Main numerical simulation --- %s seconds ---" % (time.time() - main_start))
         
@@ -76,10 +74,10 @@ def main():
                 f.write(str(row))
 
         std[i] = numAnalysis.amplitude(sol, len(t), title='Table_test_maxamp_'+str(mus[i])+'_'+str(int(L/np.pi))+'pi_'+str(i)+'.png')
-        visual.plot_video(sol, len(t), N, L, 'Table_test_'+str(int(L/np.pi))+'pi_'+str(mus[i])+'mu_'+str(i)+ '.avi', fps=30)
+        #visual.plot_video(sol, len(t), N, L, 'Table_test_'+str(int(L/np.pi))+'pi_'+str(mus[i])+'mu_'+str(i)+ '.avi', fps=30)
         #visual.plot_profile(sol, np.rint(3*dt/4), N)
-        #visual.plot_all(sol, L, T)
-    print(std[i])
+        #visual.plot_all(sol, L, T, 'Table_test_Full_Profile_'+str(int(L/np.pi))+'pi_'+str(mus[i])+'mu_'+str(i)+ '.png')
+
     #numAnalysis.simple_plot(std)    
     '''
     #############     TEST FULL SOLUTION U    #############
@@ -122,7 +120,7 @@ def main():
 class waveEquation:
     '''class waveEquation is under development and testing'''
     # The Kawahara model using Fast Fourier Transform
-    def kawahara_model(u, t,  L, param, follow_wave_profile=False):
+    def kawahara_model(u, t,  L, param, follow_wave_profile=False, damping=False):
         '''The Kawahara model using FFT
         Input:
                 u       (float)     wave amplitude
@@ -136,16 +134,22 @@ class waveEquation:
         
         alpha, beta, sigma, _, __,= param
         v0 = alpha - beta
+        if damping:                 #try to avoid uxx calculation if no damping
+            gamma = 0.1           #damping coefficient - 0.043 boundary
+            uxx = psdiff(u, period=L, order=2)      #2nd order differential
 
         #Compute the space differentials of u
         ux = psdiff(u, period=L)                #1st order differential 
         uxxx = psdiff(u, period=L, order=3)     #3rd order differential 
         uxxxxx = psdiff(u, period=L, order=5)   #5th order differential
         u2x = psdiff(u**2, period=L)            #1st order differential; accuracy needs to be confirmed
-
+        
         if follow_wave_profile:
             # moving with wave
-            dudt = v0*ux + alpha*uxxx + beta*uxxxxx + sigma*u2x #Kawahara model; moving reference frame; time differential
+            if damping:             #try to avoid uxx calculation if no damping
+                dudt = v0*ux + alpha*uxxx + beta*uxxxxx + sigma*u2x + gamma*uxx#Kawahara model; moving reference frame; time differential
+            else:
+                dudt = v0*ux + alpha*uxxx + beta*uxxxxx + sigma*u2x
         else:
             dudt = alpha*uxxx + beta*uxxxxx + sigma*u2x  
 
@@ -170,14 +174,16 @@ class waveEquation:
             a2 = -(sigma/2)*(1/(v0-4*alpha+16*beta))*a1**2                     #equation (29)
         a3 = -(sigma/2)*(1/(v0-9*alpha+81*beta))*2*a2*a1                   #equation (30)
         a4 = -(sigma/2)*(1/(v0-16*alpha+256*beta))*(a2**2+2*a2*a1)         #equation (31)
-        leading_terms = [a1, a0, a2, a3, a4]
+        leading_terms = [a0, a1, a2, a3, a4]
 
         return leading_terms
 
-    def u1_leading_terms(param=None):
+    def u1_leading_terms(leadingu0=None, param=None):
         '''Calculates the leading terms in equation (24) - the perturbation term u1
             The calculation follows the procedure described in equation (35), (36), (37), (38)
         Input:
+                leadingu0           (list)      list of u0 leading terms
+                                    default = None
                 param               (list)      list of parameters [delta, lamb, mu, alpha, beta, sigma]
                                     default = None
         Output:
@@ -186,27 +192,37 @@ class waveEquation:
         '''
         if param != None:
             delta, lamb, mu, alpha, beta, sigma = param
-        V = alpha - beta
+            V = alpha - beta
 
-        #MATRIX OPERATION
-        '''
-        #S = iD + iT
-        #Matrix D
-        D = np.zeros(8)     # only take the first 8 leading terms
-        #Matrix T
-        matrixT = np.zeros((8, 8))
-        for i in range(len(D)):
-            n = i - len(D)/2       #len(D) is guaranteed to be even
-            D[i] = (n + mu)*V - (-n + mu)**3*alpha + (n + mu)**5*beta
+            #MATRIX OPERATION
+            
+            #S = iD + iT
+            #Matrix D
+            D = np.zeros(4)     # only take the first 8 leading terms
+            #Matrix T
+            matrixT = np.zeros((4, 4))
+            for m in range(len(D)):     #loop through 0, 1, 2, 3 - total of four elements
+                D[m] = (m + mu)*V - (-m + mu)**3*alpha + (m + mu)**5*beta           #equivalent to equation (37)
+                for n in range(len(matrixT[0])):        
+                    if n != m:          #when m!=n
+                        matrixT[m][n] = 2*sigma*(mu+m)*leadingu0[abs(n-m)]          #equivalent to equation (38)
+            matrixD = np.diag(D)        #reconstruct the diagonal matrix D
 
-            for n in range(len(matrixT[0])):
-                matrixT[i][n] = 2*sigma
-        matrixD = np.diag(D)
-
-        matrixS = 1j*matrixD*1j*matrixT
-        '''
-        b1 = b2 = b3 = b4 = b5 = b6 = b7 = 0.00001         #approximate leading bs
-        leading_terms = b1, b2, b3, b4, b5, b6, b7
+            matrixS = 1j*matrixD + 1j*matrixT                                       #equation (36)
+            lambdaCalc, U1 = np.linalg.eig(matrixS)                                             #equation (35)
+            lambdaCalcMax = max(np.real(lambdaCalc))
+            indVec = np.argwhere(np.real(lambdaCalc)==lambdaCalcMax)
+            print(indVec)
+            U1 = U1[:,indVec].transpose()
+            
+            b1, b2, b3, b4 = float(np.real(U1[0][0][0])), float(np.real(U1[0][0][1])), float(np.real(U1[0][0][2])), float(np.real(U1[0][0][3]))           #calculated leading bs
+            leading_terms = b1, b2, b3, b4
+            print(leading_terms)
+            
+        else:
+            #b1 = b2 = b3 = b4 = b5 = b6 = b7 = 0.00001         #approximate leading bs
+            b1 = b2 = b3 = b4 = 0.00001         #approximate leading bs
+            leading_terms = b1, b2, b3, b4
 
         return leading_terms
 
@@ -227,7 +243,7 @@ class waveEquation:
                 u0                  (float)     amplitude
         NOTE: this stable solution has period (approximate) 2pi; under testing
         '''
-        a1, a0, a2, a3, a4 = leading_terms
+        a0, a1, a2, a3, a4 = leading_terms
 
         #u0 = a0 + a1*np.cos(2*np.pi/L*x) + a2*np.cos(2*np.pi/L*2*x) + a3*np.cos(2*np.pi/L*4*x)
         u0 = a0 + a1*np.cos(x) + a2*np.cos(2*x) + a3*np.cos(4*x)
@@ -253,13 +269,17 @@ class waveEquation:
                     tentative values:   1) stable: -0.1798; 2) unstable: 7*10**(-6) + i0.277
         '''
         delta, lamb, mu, _, __, ___,= param
-        b1, b2, b3, b4, b5, b6, b7 = leading_terms_u1
+        b1, b2, b3, b4 = leading_terms_u1
 
+        '''
         #experiment with complex calculation accuracy
         u1 = b1*np.exp(1j*(mu-1)*x) + b2*np.exp(1j*(mu-2)*x) + b3*np.exp(1j*(mu-3)*x) + b4*np.exp(1j*(mu-4)*x) + \
                 b5*np.exp(1j*(mu-5)*x) + b6*np.exp(1j*(mu-6)*x) + b7*np.exp(1j*(mu-7)*x) + \
                 b1*np.exp(1j*(mu+1)*x) + b2*np.exp(1j*(mu+2)*x) + b3*np.exp(1j*(mu+3)*x) + b4*np.exp(1j*(mu+4)*x) + \
                 b5*np.exp(1j*(mu+5)*x) + b6*np.exp(1j*(mu+6)*x) + b7*np.exp(1j*(mu+7)*x) 
+        '''
+        u1 = b1*np.exp(1j*(mu-1)*x) + b2*np.exp(1j*(mu-2)*x) + b3*np.exp(1j*(mu-3)*x) + b4*np.exp(1j*(mu-4)*x) + \
+                b1*np.exp(1j*(mu+1)*x) + b2*np.exp(1j*(mu+2)*x) + b3*np.exp(1j*(mu+3)*x) + b4*np.exp(1j*(mu+4)*x) 
 
         u = np.real(u0 + delta*np.exp(lamb*t)*u1)        # take the real part of the solution
         #print('value of u1 '+str(u))
@@ -278,30 +298,32 @@ class waveEquation:
 
         return u
 
-    def solve_kawahara(model, u0, t, L, param, steps):
+    def solve_kawahara(model, u0, t, L, param, steps, modelArg=None):
         '''Solve the Kawahara using Scipy odeint
         Input:
-            model               Kawahara model
-            u0      (array)     initial amplitude
-            t       (array)     time range
-            L       (int)       periodic range
-            param   (list)      parameters = [alpha, beta, sigma, epsilon, lamb] 
-            steps   (int)       maximum steps allowed
+            model                           Kawahara model
+            u0              (array)         initial amplitude
+            t               (array)         time range
+            L               (int)           periodic range
+            param           (list)          parameters = [alpha, beta, sigma, epsilon, lamb] 
+            steps           (int)           maximum steps allowed
+            modelArg        (list)          Kawahara model arguments
         Output:
-            sol     (array)     periodic solutions'''
+            sol             (array)         periodic solutions'''
         
-        sol = odeint(model, u0, t, args=(L, param, True), mxstep=steps)
+        sol = odeint(model, u0, t, args=(L, param, modelArg[0], modelArg[1]), mxstep=steps)
         
         return sol
 
 class visual:
     '''visual display of results'''
-    def plot_all(sol, rangeX, rangeT):
+    def plot_all(sol, rangeX, rangeT, title=None):
             '''plot the full t-x KdV solution
             Input:
                 sol         (array)
                 rangeX      (float/int)
                 rangeT      (float/int)
+                title       (str)
             Output:
                 None    
             '''
@@ -313,7 +335,10 @@ class visual:
             plt.ylabel('t')
             plt.axis('auto')
             plt.title('Kawahara on a Periodic Domain')
-            plt.show()
+            if title == None:
+                plt.savefig('full_profile_'+str(time.time())+'_'+str(id)+'.png')
+            else: 
+                plt.savefig(str(title))
 
             return 
 
@@ -356,20 +381,20 @@ class visual:
         maxAmp = max(sol[0])
         minAmp = min(sol[0])
         for i in tqdm(range(T)):      #loop through every frame
-            #if i%2 == 0:        #sample every 2 frames
-            if T <= 4000:
-                fig, ax = plt.subplots()   #initialize figure
-                ax.plot(L/N/np.pi*np.arange(N), sol[i])
-                ax.set_xlabel('Position x (*pi)')
-                ax.set_ylabel('Amplitude')
-                ax.set_ylim(1.3*minAmp, 1.1*maxAmp)
-                ax.set_title('Wave Profile at Time Step: ' + str(i))
-                images = imv.memory.savebuff(plt, images)       #save image to the temp container
-                plt.close()
-            else:
-                print('Total number of frames - ' + str(T))
-                batch = int(np.ceil(T/4000))
-                print('Devided in ' + str(batch) + 'batches.')
+            if i%2 == 0:        #sample every 2 frames
+                if T <= 4000:
+                    fig, ax = plt.subplots()   #initialize figure
+                    ax.plot(L/N/np.pi*np.arange(N), sol[i])
+                    ax.set_xlabel('Position x (*pi)')
+                    ax.set_ylabel('Amplitude')
+                    ax.set_ylim(1.3*minAmp, 1.1*maxAmp)
+                    ax.set_title('Wave Profile at Time Step: ' + str(i))
+                    images = imv.memory.savebuff(plt, images)       #save image to the temp container
+                    plt.close()
+                else:
+                    print('Total number of frames - ' + str(T))
+                    batch = int(np.ceil(T/4000))
+                    print('Devided in ' + str(batch) + 'batches.')
 
         imv.memory.construct(images, str(title), fps, inspect=False)        #construct the video 
 
