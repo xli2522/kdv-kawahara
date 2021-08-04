0112ab794614c4e13fba3938e7ebb762d4a1185f
xli2522 <50414011+xli2522@users.noreply.github.com>
2021-08-04 14:56:27 -0400

    damping kawahara model params fix and table 1

--- Kawahara/run_kawahara_mutiprocess.py
+++ Kawahara/run_kawahara_mutiprocess.py
@@ -14,6 +14,7 @@ def full_operation(pair):
     a1=0.001
     #a1 = 0.373
     damp_all_cases = False
+    damp_all_cases_analytical = False
     optimize_stable = False
     optimize_unstable = False
 
@@ -32,15 +33,15 @@ def full_operation(pair):
     dx = L / (N - 1.0)                      #spatial step size
     x = np.linspace(0, (1-1.0/N)*L, N)      #initialize x spatial axis    
     # Set the time sample grid.
-    T = 8
-    t = np.linspace(0, T, 600)
+    T = 24
+    t = np.linspace(0, T, 1500)
     dt = len(t)
 
     ######################################################################
-    param_damping = [1, beta, 1, 0.01]
+    param_damping = [1, beta, 1, a1]
     param_no_damping = [1, beta, 1]
        
-    if damp_all_cases:
+    if damp_all_cases_analytical:
         #this is incorrect atm; find_stable_lamb outputs the Re{lambda} instead of the Im{lambda}
         lamb, gamma = analytical.find_stable_lamb(param_damping, a1, mu, 800, savePic=True, plot=False)
         param_damping = [1, beta, 1, gamma]
@@ -57,12 +58,12 @@ def full_operation(pair):
     #continue
     
     if optimize_unstable:
-        a1 = analytical.optimize_Floquet_a1(param_no_damping, 0.001, 1, mu, 1000, stable=False, savePic=True)
+        a1 = analytical.optimize_Floquet_a1(param_no_damping, 0.1, 1.1, mu, 100, stable=False, savePic=True)
     elif optimize_stable:
-        a1 = analytical.optimize_Floquet_a1(param_no_damping, 0.9, 1.1, mu, 500, stable=True, savePic=True)
+        a1 = analytical.optimize_Floquet_a1(param_no_damping, 0.0009, 0.1, mu, 1000, stable=True, savePic=True)
     else:
         a1 = a1
-        #lamb_no_dampRe, lamb_no_dampIm = analytical.lambdaFloquet(0.97*mu, 1.03*mu, 200, a1, param_no_damping, mu, savePic=True, damping=damp_all_cases)
+        lamb_no_dampRe, lamb_no_dampIm = analytical.lambdaFloquet(0.97*mu, 1.03*mu, 200, a1, param_no_damping, mu, savePic=True, damping=damp_all_cases_analytical)
 
     if a1 == None:
         pass
@@ -72,7 +73,7 @@ def full_operation(pair):
     #continue
     ######################################################################
     param1 = [1, beta, 1, 0.01, lamb]                               #[alpha, beta, sigma, epsilon, lamb]
-    param2 = [a1, lamb, mu, 1, beta, 1]                           #[delta, lamb, mu, alpha, beta, sigma]
+    param2 = [a1, lamb, mu, 1, beta, 1]                             #[delta, lamb, mu, alpha, beta, sigma]
     #a1 = 0.001   #param1[3]                                        #DECREASE A1 WHEN BETA IS LARGE
 
     kModes=10                                                       #number of fourier modes to consider
@@ -80,17 +81,17 @@ def full_operation(pair):
     
     ###########################     U0  aN    ###########################
     #approximate the optimized u0 coeficients for analytical approximation of perturbation solution u1
-    if damp_all_cases:
+    if damp_all_cases_analytical:
         optimized_u0, v0, stationary_u0 = analytical.optimize_u0Coeff(param_damping, a1, a1, 
-                        steps=1, L=L, spaceResolution=N, plot=False, N=2*kModes+1, savePic=False, damping=damp_all_cases)
-        lambdaCalc, U1 = analytical.fourierCoeffMatrix(param_damping, optimized_u0, v0, mu, damping=damp_all_cases)
+                        steps=1, L=L, spaceResolution=N, plot=False, N=2*kModes+1, savePic=False, damping=damp_all_cases_analytical)
+        lambdaCalc, U1 = analytical.fourierCoeffMatrix(param_damping, optimized_u0, v0, mu, damping=damp_all_cases_analytical)
     else:
         optimized_u0, v0, stationary_u0 = analytical.optimize_u0Coeff(param_no_damping, a1, a1, 
-                        steps=1, L=L, spaceResolution=N, plot=False, N=2*kModes+1, savePic=False, damping=damp_all_cases)
+                        steps=1, L=L, spaceResolution=N, plot=False, N=2*kModes+1, savePic=False, damping=damp_all_cases_analytical)
 
         ###########################     U1  aN    ###########################
         #calculate the leading terms of the perturbation solution u1
-        lambdaCalc, U1 = analytical.fourierCoeffMatrix(param_no_damping, optimized_u0, v0, mu, kModes=kModes, damping=damp_all_cases)
+        lambdaCalc, U1 = analytical.fourierCoeffMatrix(param_no_damping, optimized_u0, v0, mu, kModes=kModes, damping=damp_all_cases_analytical)
     
     #calculate the perturbation solution u1
     perturbation_u1 = analytical.collect_u1(lambdaCalc, lamb, U1, mu, x)
@@ -98,20 +99,20 @@ def full_operation(pair):
     ###########################     Uc  wE    ###########################
     #calculate the combined solution u
     
-    combined_u =  waveEquation.kawahara_combined_solution(stationary_u0, x, param2, 0.01, a1, u1=perturbation_u1)           
+    combined_u =  waveEquation.kawahara_combined_solution(stationary_u0, x, param2, a1, a1, u1=perturbation_u1)           
     #visual.plotInitial(combined_u, mu, a1, 0)
                  
     ###########################     Solve     ###########################
     print("Initial condition calculation --- %s seconds ---" % (time.time() - ic_start))
     solver_start = time.time()
-    sol = waveEquation.solve_kawahara(waveEquation.kawahara_model, combined_u, t, L, param1, 3000, modelArg=(True, damp_all_cases, v0))
+    sol = waveEquation.solve_kawahara(waveEquation.kawahara_model, combined_u, t, L, param1, 3000, modelArg=(True, damp_all_cases, a1, v0))
     print("Numerical solver --- %s seconds ---" % (time.time() - solver_start))
     print("Main numerical simulation --- %s seconds ---" % (time.time() - main_start))
     
 
     avg = numAnalysis.amplitude(sol, len(t), title='Table_test_'+str(a1)+'maxamp_'+str(mu)+'_'+str(int(L/np.pi))+'pi'+'.png')
     #continue
-    #visual.plot_video(sol, len(t), N, L, 'Table_test_'+str(int(L/np.pi))+'pi_'+str(mus)+'mu'+ '.avi', fps=30)
+    #visual.plot_video(sol, len(t), N, L, 'Table_test_'+str(int(L/np.pi))+'pi_'+str(mu)+'mu'+ '.avi', fps=30)
     
     #############                             #############
     #############     Numerical Instability   #############
@@ -121,10 +122,12 @@ if __name__ == '__main__':
     #mus = [5.09,5.48,4.79,5.02,4.16,4.23,3.24,-3.23,2.27,2.36,1.49,1.64,0.74,0.69]
     #lambs = [62.82,51.62,35.98,19.78,7.09,0.595,-0.219,-0.305,-3.22,-13.41,-29.71,-49.13,-64.87,-57.60]
     current = time.time()
-    pairs = [[5.09,62.82], [5.48,51.62], [4.79,35.98], [5.02,19.78], [4.16,7.09], [4.23,0.595], [3.24,-0.219], \
-                    [-3.23,-0.305], [2.27,-3.22], [2.36,-13.41], [1.49,-29.71], [1.64,-49.13], [0.74,-64.87], [0.69, -57.60]]
-    pool = Pool(processes=14)
-    pool.map(full_operation, pairs)
+    pairs1 = [[0.7845, -0.1798], [0.6324,0.2277 ], [-0.7928, 0.2128]]       #beta = 1/4
+    pairs2 = [[5.09,62.82], [5.48,51.62], [4.79,35.98], [5.02,19.78], [4.16,7.09], [4.23,0.595], [3.24,-0.219], \
+                    [-3.23,-0.305], [2.27,-3.22], [2.36,-13.41], [1.49,-29.71], [1.64,-49.13], [0.74,-64.87], [0.69, -57.60]]           #beta = 3/160
+    
+    pool = Pool(processes=8)
+    pool.map(full_operation, pairs2)
     pool.terminate()
     print('Full operation completed in ---- ' + str(time.time() - current) + ' ----')
 
